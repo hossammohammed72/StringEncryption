b855066f5066fd981597033a02da7cf2d5e1c7ca
Hossam <hossammohammed72@gmail.com>
2019-11-17 22:42:01 +0200

    Implement Decrypt algorithm for “Shift Algorithm” [Fourth Commit]

---
+++ app/Services/ShiftDecryptionAlgorithmService.php
@@ -0,0 +1,22 @@
+<?php 
+namespace App\Services;
+
+class ShiftDecryptionAlgorithmService {
+
+    public function decrypt(String $string): String
+    {
+        for( $i = 0 ; $i<strlen($string) ; $i++){
+            $asciiValue = ord($string[$i]);
+            if( ($asciiValue >= 65 && $asciiValue <=  90 ) || ($asciiValue >= 97 && $asciiValue <=  122) ){
+                $string[$i] = chr($asciiValue-3);
+                if(ord($string[$i]) < 65 || ( ord($string[$i]) > 90 && ord($string[$i]) < 97) ){    
+                    $string[$i] = chr(ord($string[$i])+26);    
+                }
+            }else{
+                continue;
+            }
+        }   
+        return $string;
+    }
+}
+?>
